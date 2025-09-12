fe201d383ad47f41d56af7e5aec1f695412e0f0c
SameerBalehosur <sameerbalehosur143@gmail.com>
2025-09-12 12:10:40 +0530

    test cases added

--- src/test/java/com/igq/product_service/controllers/ProductControllerTest.java
+++ src/test/java/com/igq/product_service/controllers/ProductControllerTest.java
@@ -2,6 +2,7 @@ package com.igq.product_service.controllers;
 
 import com.igq.product_service.constants.ProductsConstants;
 import com.igq.product_service.dto.ProductRequest;
+import com.igq.product_service.dto.ProductResponse;
 import com.igq.product_service.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -40,6 +43,41 @@ class ProductControllerTest {
         assertEquals(HttpStatus.CREATED,product.getStatusCode());
         assertEquals(ProductsConstants.CREATED,product.getBody());
         Mockito.verify(productService,Mockito.times(1)).createProduct(request);
+
+    }
+    @Test
+    void createProductFail() {
+        ProductRequest request = new ProductRequest();
+        // Mock service behavior
+
+        Mockito.when(productService.createProduct(request)).thenReturn(String.valueOf(HttpStatus.BAD_REQUEST));
+        ResponseEntity<?> product = productController.createProduct(request);
+        assertEquals(HttpStatus.BAD_REQUEST,product.getStatusCode());
+        Mockito.verify(productService,Mockito.times(1)).createProduct(request);
+
+    }
+
+    @Test
+    public void getAllProductSuccess() {
+        ProductResponse request = new ProductResponse();
+        request.setName("Sameer");
+        request.setDescription("High-end gaming laptop");
+        request.setPrice(BigDecimal.valueOf(1200.0));
+        List<ProductResponse> productResponses = new ArrayList<>();
+        productResponses.add(request);
+        Mockito.when(productService.getAllProduct()).thenReturn(productResponses);
+        ResponseEntity<?> allProduct = productController.getAllProduct();
+        assertEquals(HttpStatus.OK, allProduct.getStatusCode());
+        Mockito.verify(productService,Mockito.times(1)).getAllProduct();
+
+    }
+    @Test
+    public void getAllProductEmptyList() {
+        List<ProductResponse> productResponses = new ArrayList<>();
+        Mockito.when(productService.getAllProduct()).thenReturn(productResponses);
+        ResponseEntity<?> allProduct = productController.getAllProduct();
+        assertEquals(HttpStatus.NOT_FOUND, allProduct.getStatusCode());
+        Mockito.verify(productService,Mockito.times(1)).getAllProduct();
 
     }
 
